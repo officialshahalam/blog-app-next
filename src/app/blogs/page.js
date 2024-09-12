b2526bee54aca4f71df4ba039967be6ea2701e75
Mohd Shahalam <officialshahalam855@gmail.com>
2024-09-12 08:51:47 +0530

    update get url

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,9 +1,11 @@
 import BlogOverview from "@/components/blogOverView/page";
 
 async function fetchAllBlogs() {
-    const BASE_URL=process.env.GET_BLOG_BASE_URL
+    const GET_URl=`${process.env.GET_BLOG_BASE_URL}/api/get-blogs`;
+    console.log("GET_URL is:::",GET_URl);
     try {
-        const apiResponse = await fetch(`${BASE_URL}/api/get-blogs`, {
+
+        const apiResponse = await fetch(GET_URl, {
             method: "GET",
             cache:"no-cache"
         });
@@ -17,9 +19,7 @@ async function fetchAllBlogs() {
 
 
 const Blogs = async () => {
-
     const allBlogs = await fetchAllBlogs();
-
     return (
         <div className="min-h-screen bg-gradient-to-tr from-violet-400 via-violet-600 to-violet-700">
             <BlogOverview allBlogs={allBlogs} />
